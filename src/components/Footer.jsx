b1eea4d8e7f1de3cb244aabc617e83b610e3a37b
Sipho Nkebe <siphonkebe@gmail.com>
2025-02-11 08:52:33 +0200

    'updates'

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#333] py-6">
       <div className="container mx-auto text-center">
-        <p className="text-gray-300 text-xs">&copy; {new Date().getFullYear()} &#124; All Rights Reserved</p>
+        <p className="text-gray-300 text-xs">&copy; {new Date().getFullYear()} &#124; Sipho Nkebe</p>
       </div>
     </footer>
   );
